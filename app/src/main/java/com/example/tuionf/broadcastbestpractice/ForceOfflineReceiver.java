d2b69af6a8de61d6dcbdcebbe43c00421a4cc52e
tuionf <tuionf619@gmail.com>
2016-01-24 12:18:24 +0800

    去除修改bug时的Log方法

--- app/src/main/java/com/example/tuionf/broadcastbestpractice/ForceOfflineReceiver.java
+++ app/src/main/java/com/example/tuionf/broadcastbestpractice/ForceOfflineReceiver.java
@@ -13,21 +13,15 @@ public class ForceOfflineReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
 
-        Log.d("ForceOfflineReceiver","jie shou dao");
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
-        Log.d("ForceOfflineReceiver","new");
         dialogBuilder.setTitle("Warning");
-        Log.d("ForceOfflineReceiver","setTitle");
         dialogBuilder.setMessage("You are forced to be offline.Please try to login again.");
-        Log.d("ForceOfflineReceiver","setMessage");
         dialogBuilder.setCancelable(false);
-        Log.d("ForceOfflineReceiver","setCancelable");
-
 
         dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Log.d("ForceOfflineReceiver","dian ji shi jian");
+               ;
 
                 //销毁所有的活动
                 ActivityCollector.finishAll();
@@ -38,10 +32,10 @@ public class ForceOfflineReceiver extends BroadcastReceiver {
         });
         AlertDialog alertDialog = dialogBuilder.create();
 
-        Log.d("ForceOfflineReceiver","dialogBuilder.create");
+
         //需要设置 AlertDialog 的类型，保证在广播接收器中可以正常弹出
         alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
         alertDialog.show();
-        Log.d("ForceOfflineReceiver","show");
+
     }
 }

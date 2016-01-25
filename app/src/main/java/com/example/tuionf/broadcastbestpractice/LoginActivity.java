b58d20159df932d6e75e6850dc540182cdbead8e
tuionf <tuionf619@gmail.com>
2016-01-25 19:13:42 +0800

    去除修改bug时的Log方法
    新增记住密码的功能

--- app/src/main/java/com/example/tuionf/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/example/tuionf/broadcastbestpractice/LoginActivity.java
@@ -1,16 +1,25 @@
 package com.example.tuionf.broadcastbestpractice;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends Activity {
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
+
+    private SharedPreferences sharedPreferences;
+    private CheckBox checkBox;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,26 @@ public class LoginActivity extends BaseActivity {
         accountEdit =(EditText) findViewById(R.id.account);
         passwordEdit =(EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
+        checkBox = (CheckBox) findViewById(R.id.remember_checkbox);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isRemember = sharedPreferences.getBoolean("remember_password",false);
+
+        if (isRemember){
+            Log.e("Login",isRemember+"");
+            //将账号和密码都设置到文本框中
+            String account = sharedPreferences.getString("account","");
+            String password = sharedPreferences.getString("password","");
+            Log.e("Login","account   "+account);
+            Log.e("Login","password  "+password);
+
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            checkBox.setChecked(true);
+            checkBox.isChecked();
+        }
+
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +58,20 @@ public class LoginActivity extends BaseActivity {
 
                 //如果账号密码是 tuionf 123456，则认为登陆成功
                 if(account.equals("tuionf") && password.equals("123456")){
+                    editor = sharedPreferences.edit();
+                    if (checkBox.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                   }else{
+                        editor.clear();
+                    }
+
+                    editor.commit();
+
+                    Log.e("Login","account——"+account +"——password——"+password );
+                    Log.e("Login", sharedPreferences.getBoolean("remember_password",false)+"");
+
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
